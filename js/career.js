0f229fcb86ea5e55024e6a5e033e454cde06c890
Hell awakes <HellAwakesStudio10@gmail.com>
2025-01-01 02:53:05 +0100

    dw

---
+++ js/career.js
@@ -0,0 +1,22 @@
+function Show() {
+    document.getElementById("div1").style.display = "block";
+    document.getElementById("div2").style.display = "block";
+    document.getElementById("div3").style.display = "block";
+
+}
+
+function website() {
+    document.getElementById("div1").style.display = "none";
+    document.getElementById("div2").style.display = "block";
+    document.getElementById("div3").style.display = "none";
+}
+function Roblox() {
+    document.getElementById("div1").style.display = "block";
+    document.getElementById("div2").style.display = "none";
+    document.getElementById("div3").style.display = "none";
+}
+function Tester() {
+    document.getElementById("div1").style.display = "none";
+    document.getElementById("div2").style.display = "none";
+    document.getElementById("div3").style.display = "block";
+}
